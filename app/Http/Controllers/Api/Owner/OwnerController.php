03126653b22e0187c2eb518b57ef8b9f54448278
Alana1252 <alankamesta.ginting@gmail.com>
2025-05-09 21:45:08 +0700

    kn

--- app/Http/Controllers/Api/Owner/OwnerController.php
+++ app/Http/Controllers/Api/Owner/OwnerController.php
@@ -5,23 +5,21 @@ namespace App\Http\Controllers\Api\Owner;
 use App\Http\Controllers\Controller;
 use App\Models\Pesanan;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class OwnerController extends Controller
 {
     public function lihatSemuaDetail()
     {
-        $owner = Auth::user();
-        $pesanan = Pesanan::with(['paket', 'user', 'jadwal'])->get();
+        $groupedPesanan = Pesanan::with(['user', 'paket', 'jadwal'])
+            ->get()
+            ->groupBy('status'); // Mengelompokkan berdasarkan status pesanan
+    
         return response()->json([
             'success' => true,
-            'message' => 'Owner data retrieved successfully',
-            'data' => [
-                'nama_pemesan' => $pesanan->user->name,
-                'nama_paket' => $pesanan->paket->nama_paket,
-                'tanggal_pemesanan' => $pesanan->tanggal_pemesanan,
-                'status_pemesanan' => $pesanan->status_pemesanan,
-            ]
+            'message' => 'Data pesanan berhasil diambil dan dikelompokkan berdasarkan status',
+            'data' => $groupedPesanan,
         ]);
     }
+    
+
 }
